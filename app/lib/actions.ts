aba3e11a7b4008fa9ff230971058f794c092873f
Pete Warnock <github@peterwarnock.com>
2024-11-10 15:28:50 -0800

    remove demo error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,9 +64,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
- 
-  // Unreachable code block
+
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
